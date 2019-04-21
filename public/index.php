c2f007f11a645cc846435a114c36eec3535d9a6e
cjwood032 <cjwood032@gmail.com>
2019-04-21 08:14:25 -0400

    basic files added

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <title>Delta Defense Coding Challenge</title>
+    <meta charset="utf-8">
+  </head>
+
+  <body>
+
+    <h1>Delta Defense Project: Coming Soon</h1>
+    <a href ="./topics/index.php">Topics</a>
+    <a href = "#">User</a>
+  </body>
+</html>
---
+++ public/topics/index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <title>Topics</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" media="all" href="../stylesheets/topics.css" />
+  </head>
+
+  <body>
+    <header>
+        <h1>Discussion Topics</h1>
+    </header>
+
+    <navigation>
+        <ul>
+        <li><a href ="index.php">Menu</a></li>
+        </ul>
+    </navigation>
+
+    <div id="content">
+    </div>
+    <footer>
+        &copy; <?php echo date('Y')?> Wood
+    </footer>
+  </body>
+</html>
