d08c78463f9f6d158fbd8d1cf9c798d46abd741c
cjwood032 <cjwood032@gmail.com>
2019-04-21 14:31:54 -0400

    connected to mysqli

---
+++ private/db_credentials.php
@@ -0,0 +1,8 @@
+<?php
+
+define("DB_SERVER", "127.0.0.1");
+define("DB_USER", "deltademo");
+define("DB_PASS", "homesecurity");
+define("DB_NAME", "delta_defense");
+
+?>
--- public/topics/edit.php
+++ public/topics/edit.php
@@ -1,4 +1,28 @@
-<?php require_once('../../private/initialize.php'); ?>
+<?php 
+    require_once('../../private/initialize.php');
+    if(!isset($_GET['id'])) {
+        redirect_to(url_for('/topics/index.php'));
+      }
+    $id = $_GET['id'];
+    $title= '';
+    $post_body='Enter your post here';
+    if(is_post_request()) {
+
+        // Handle form values sent by new.php
+      
+        $title = $_POST['title'] ?? '';
+        $post_body = $_POST['post_body'] ?? '';
+      
+        echo "Form parameters<br />";
+        echo "Title: " . $title . "<br />";
+        echo "Post Body: " . $post_body . "<br />";
+      } else {
+        //redirect_to(url_for('/topics/new.php'));
+      }
+      
+?>
+      
+
 <?php $page_title = 'Edit Topic'; ?>
 <?php include(SHARED_PATH . '/topics_header.php'); ?>
 <?php 
@@ -6,13 +30,10 @@
   echo h($id); 
 ?>
 <h2>Edit</h2>
-<form action="" method="post">
+<form action="<?php echo url_for('/topics/edit.php?id=' . h(u($id))); ?>" method="post">
     
-    Title:<input type="text" name="title" value ="" /> <input type="submit" value="Create Topic" /><br/>
-    <textarea name="post_body" value ="" rows="10" cols="30">
-    Enter your post here:
-    </textarea>
-    
-    
+    Title:<input type="text" name="title" value ="<?php echo $title?>" /> <input type="submit" value="Create Topic" /><br/>
+    <textarea name="post_body" value ="" rows="10" cols="30"><?php echo $post_body?></textarea>
+      
 </form>
 <?php include(SHARED_PATH . '/topics_footer.php'); ?>
